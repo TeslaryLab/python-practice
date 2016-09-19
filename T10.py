ef20bdf7b2b5f787b2e27513b9a4efadcc0cae1e
73F <mxl73@outlook.com>
2016-09-20 01:27:07 +0800

    切片

---
+++ T10.py
@@ -0,0 +1,14 @@
+"""分片"""
+
+if __name__ == '__main__':
+	example = ['a','b','c','d','e','f','g','h']
+	print(example[2:5])
+	print(example[-3:])
+	print(example[3:-2])
+	print(example[-3:2])
+	print(example[100:101])
+"""
+切片比较简单
+另外切片不会出现异常
+it's easy
+"""
